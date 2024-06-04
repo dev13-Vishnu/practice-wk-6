a36e70d5bd4ea4fd1368ecdbfd2e1ac89cd0e97d
Vishnu P S <vishnu13three@gamil.com>
2024-06-04 10:09:55 +0530

    the sum of user input

--- Javascript/sum_of_user_input.js
+++ Javascript/sum_of_user_input.js
@@ -1,3 +1,8 @@
-const readLine = requieres ("readline-sync");
+const readLine = require ("readline-sync");
 
-const num1 
+const num1 = parseFloat(readLine.question('Enter the firs number: '));
+
+const num2 = parseFloat(readLine.question('Enter the second number: '));
+
+const sum = num1+ num2;
+console.log("The sum is : " +sum);
